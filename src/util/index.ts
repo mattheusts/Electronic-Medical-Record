29dcf2e1a54250423c33fe95ee3e5e729a2db3f8
matthaw <hardway3@protonmail.com>
2021-03-19 18:17:45 -0300

    feat: utils types

---
+++ src/util/index.ts
@@ -0,0 +1,33 @@
+import { Prescription } from '../database/models/Prescription';
+
+export interface usersAndPrescription {
+  id: string;
+  name: string;
+  prescription: string;
+}
+
+export interface UserAndPrescriptions {
+  id?: string;
+  name: string;
+  naturalness: string;
+  mother: string;
+  dad: string;
+  sex: string;
+  birth: string;
+  prescriptions: Prescription[];
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface UserAndPrescription {
+  id?: string;
+  name: string;
+  naturalness: string;
+  mother: string;
+  dad: string;
+  sex: string;
+  birth: string;
+  prescription: Prescription;
+  created_at?: Date;
+  updated_at?: Date;
+}
