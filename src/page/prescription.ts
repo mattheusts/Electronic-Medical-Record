0c81d83ee758c85c70ec0dcb2e6927acfa9b8d9c
matthaw <hardway3@protonmail.com>
2021-04-01 16:18:46 -0300

    feat: adding new inputs and gallery

--- src/page/prescription.ts
+++ src/page/prescription.ts
@@ -1,6 +1,9 @@
 import { ipcRenderer } from 'electron';
 import { Prescription } from '../database/models/Prescription';
 import { UserAndPrescriptions } from '../util';
+import { PhotoUpload, PhotoInfo } from '../util/Photo';
+
+export const photosUpload = new PhotoUpload();
 
 let id = '';
 const name = document.getElementById('name') as HTMLElement;
@@ -19,6 +22,52 @@ window.onload = () => {
 
 savePrescriptions.addEventListener('click', function (event: Event) {
   event.preventDefault();
+  const main_complaint = document.getElementById(
+    'main_complaint'
+  ) as HTMLInputElement;
+  const history_current = document.getElementById(
+    'history_current'
+  ) as HTMLInputElement;
+  const physical_exam = document.getElementById(
+    'physical_exam'
+  ) as HTMLInputElement;
+  const cardiovascular_system = document.getElementById(
+    'cardiovascular_system'
+  ) as HTMLInputElement;
+  const blood_pressure = document.getElementById(
+    'blood_pressure'
+  ) as HTMLInputElement;
+  const heart_rate = document.getElementById('heart_rate') as HTMLInputElement;
+  const respiratory_system = document.getElementById(
+    'respiratory_system'
+  ) as HTMLInputElement;
+  const vesicular_murmur = document.getElementById(
+    'vesicular_murmur'
+  ) as HTMLInputElement;
+  const oxygen_Saturation = document.getElementById(
+    'oxygen_Saturation'
+  ) as HTMLInputElement;
+  const adb = document.getElementById('adb') as HTMLInputElement;
+  const upper_limbs = document.getElementById(
+    'upper_limbs'
+  ) as HTMLInputElement;
+  const previous_pathological_history = document.getElementById(
+    'previous_pathological_history'
+  ) as HTMLInputElement;
+  const neurological_examination = document.getElementById(
+    'neurological_examination'
+  ) as HTMLInputElement;
+  const family_history = document.getElementById(
+    'family_history'
+  ) as HTMLInputElement;
+  const social_history = document.getElementById(
+    'social_history'
+  ) as HTMLInputElement;
+  const physiological_history = document.getElementById(
+    'physiological_history'
+  ) as HTMLInputElement;
+  const ducts = document.getElementById('ducts') as HTMLInputElement;
+
   const prescriptionDate = document.getElementById(
     'prescription_date'
   ) as HTMLInputElement;
@@ -27,6 +76,23 @@ savePrescriptions.addEventListener('click', function (event: Event) {
   ) as HTMLInputElement;
 
   const res: Prescription = {
+    main_complaint: main_complaint.value,
+    history_current: history_current.value,
+    physical_exam: physical_exam.value,
+    cardiovascular_system: cardiovascular_system.value,
+    blood_pressure: blood_pressure.value,
+    heart_rate: heart_rate.value,
+    respiratory_system: respiratory_system.value,
+    vesicular_murmur: vesicular_murmur.value,
+    oxygen_Saturation: oxygen_Saturation.value,
+    adb: adb.value,
+    upper_limbs: upper_limbs.value,
+    previous_pathological_history: previous_pathological_history.value,
+    neurological_examination: neurological_examination.value,
+    family_history: family_history.value,
+    social_history: social_history.value,
+    physiological_history: physiological_history.value,
+    ducts: ducts.value,
     prescription_date: prescriptionDate.value,
     prescription: prescription.value,
     user_id: id,
@@ -77,3 +143,34 @@ ipcRenderer.on(
     }
   }
 );
+
+// Gallery
+document.getElementById('files').onchange = function () {
+  const gallery = document.getElementById('gallery') as HTMLElement;
+  gallery.innerHTML = '';
+
+  const fileList = Array.from(this.files);
+  const files: PhotoInfo[] = fileList.map((file: any) => {
+    return {
+      name: file.name,
+      path: file.path,
+      size: file.size,
+      type: file.type,
+      lastModified: file.lastModified,
+      lastModifiedDate: file.lastModifiedDate,
+    };
+  });
+
+  photosUpload.addFile(files);
+
+  gallery.appendChild(photosUpload.render());
+};
+
+export function deleteFile(name: string): void {
+  const gallery = document.getElementById('gallery') as HTMLElement;
+  gallery.innerHTML = '';
+
+  photosUpload.removeFileByName(name);
+
+  gallery.appendChild(photosUpload.render());
+}
