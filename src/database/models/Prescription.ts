0244e997fca40d70fc88a9597f208034daf833a0
matthaw <hardway3@protonmail.com>
2021-04-02 19:50:16 -0300

    feat: add new columns

--- src/database/models/Prescription.ts
+++ src/database/models/Prescription.ts
@@ -13,6 +13,57 @@ export class Prescription {
   readonly id?: string;
 
   @Column()
+  main_complaint: string;
+
+  @Column()
+  history_current: string;
+
+  @Column()
+  physical_exam: string;
+
+  @Column()
+  cardiovascular_system: string;
+
+  @Column()
+  blood_pressure: string;
+
+  @Column()
+  heart_rate: string;
+
+  @Column()
+  respiratory_system: string;
+
+  @Column()
+  vesicular_murmur: string;
+
+  @Column()
+  oxygen_Saturation: string;
+
+  @Column()
+  adb: string;
+
+  @Column()
+  upper_limbs: string;
+
+  @Column()
+  previous_pathological_history: string;
+
+  @Column()
+  neurological_examination: string;
+
+  @Column()
+  family_history: string;
+
+  @Column()
+  social_history: string;
+
+  @Column()
+  physiological_history: string;
+
+  @Column()
+  ducts: string;
+
+  @Column()
   prescription: string;
 
   @Column()
