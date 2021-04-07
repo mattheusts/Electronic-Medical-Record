99e0e2b84ee0af1180516e6005531ab8ebab1a99
matthaw <hardway3@protonmail.com>
2021-04-07 18:13:34 -0300

    feat: render old prescriptions

--- src/page/render.ts
+++ src/page/render.ts
@@ -1,3 +1,4 @@
+import { UserAndPrescriptions } from '../util';
 import { UserSearch } from '../database/search';
 
 export class Render {
@@ -38,21 +39,72 @@ export class Render {
 
       render.appendChild(card);
 
-      //   render += `
-      //   <div class="card user-card-full">
-      //       <div class="card-body">
-      //         <h5 class="m-b-10 f-w-600">${u.name}</h5>
-      //         <p class="card-text text-muted f-w-400">
-      //           Data de Nascimento: ${u.birth}
-      //           <br>
-      //         </p>
-      //         <button type="button" class="btn btn-primary" onclick="edit('${u.id}')">Visualizar</button>
-      //       </div>
-      //     </div>
-      //   `;
-      // }
-
       return render;
     }
+  }
+
+  public static renderOldPrescriptions(
+    userAndPrescriptions: UserAndPrescriptions
+  ): HTMLElement {
+    const root: HTMLElement = document.createElement('div');
+
+    if (userAndPrescriptions.prescriptions == []) {
+      const render = document.createElement('h6');
+      render.className = 'f-w-400';
+      render.innerText = 'Nenhuma prescrição feita';
+      return render;
+    } else {
+      for (const prescription of userAndPrescriptions.prescriptions) {
+        root.className = 'row d-inline';
+        const render = document.createElement('div');
+        render.className = 'row d-inline';
+
+        const divButtons = document.createElement('div');
+        divButtons.className = 'aling-right-buttons';
+
+        const editButton = document.createElement('button');
+        editButton.innerText = 'Editar';
+        editButton.className = 'btn btn-primary btn-sm';
+        editButton.setAttribute('onclick', `edit('${prescription.id}')`);
+
+        const printButton = document.createElement('button');
+        printButton.innerText = 'Imprimir';
+        printButton.className = 'btn btn-success btn-sm';
+        printButton.setAttribute('onclick', `print('${prescription.id}')`);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Deletar';
+        deleteButton.className = 'btn btn-danger btn-sm';
+        deleteButton.setAttribute('onclick', `delete('${prescription.id}')`);
+
+        divButtons.appendChild(editButton);
+        divButtons.appendChild(printButton);
+        divButtons.appendChild(deleteButton);
+
+        const prescriptionElement = document.createElement('p');
+        prescriptionElement.className = 'm-b-10 f-w-600 d-inline';
+        prescriptionElement.innerText = prescription.prescription;
+
+        const spanDateElement = document.createElement('span');
+        spanDateElement.className = 'text-muted f-w-400';
+        spanDateElement.innerText = prescription.prescription_date;
+
+        const h6Element = document.createElement('h6');
+        h6Element.className = 'f-w-400';
+        h6Element.innerText = 'Prescrição feita em ';
+        h6Element.appendChild(spanDateElement);
+
+        const endLineH6Element = document.createElement('h6');
+        endLineH6Element.className = 'm-b-20 m-t-40 p-b-5 b-b-default f-w-600';
+
+        render.appendChild(divButtons);
+        render.appendChild(prescriptionElement);
+        render.appendChild(h6Element);
+        render.appendChild(endLineH6Element);
+
+        root.appendChild(render);
+      }
+    }
+    return root;
   }
 }
