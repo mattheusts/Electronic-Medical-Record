e4b11bd1df38a424693571713646613617a56a16
matthaw <hardway3@protonmail.com>
2021-04-09 16:56:24 -0300

    feat: new logic of operating instances

---
+++ src/page/operations.ts
@@ -0,0 +1,29 @@
+import { ipcRenderer } from 'electron';
+
+export function editUser(id: string): void {
+  ipcRenderer.send('editUser', id);
+}
+
+export function newPrescription(id: string): void {
+  ipcRenderer.send('newPrescriptionInit', id);
+}
+
+export function printPDF(id: string): void {
+  ipcRenderer.send('printPDF', id);
+}
+
+export function deleteUser(id: string): void {
+  ipcRenderer.send('deleteUser', id);
+}
+
+export function deletePrescription(id: string): void {
+  ipcRenderer.send('deletePrescription', id);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export function editPrescription(
+  prescription_id: string,
+  user_id: string
+): void {
+  ipcRenderer.send('editPrescription', { user_id, prescription_id });
+}
