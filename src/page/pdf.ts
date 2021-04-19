f585bc3a8aff0bf408d645324b0995ff3b03c883
matthaw <hardway3@protonmail.com>
2021-04-19 18:02:01 -0300

    feat: timeout for viwer pdf

--- src/page/pdf.ts
+++ src/page/pdf.ts
@@ -1,14 +1,21 @@
 import { ipcRenderer } from 'electron';
 
-window.onload;
+window.onload = () => {
+  console.log('loaded');
+};
 
-ipcRenderer.on('showPDF', (event, res: string) => {
+ipcRenderer.on('showPDF', async (event, res: string) => {
   const container = document.getElementById('root') as HTMLInputElement;
+  console.log(res);
+  setTimeout(() => {
+    const embed = document.createElement('iframe');
+    embed.setAttribute('src', res);
+    embed.setAttribute('width', '500');
+    embed.setAttribute('height', '500');
+    embed.setAttribute('style', 'overflow: hidden');
+    container.appendChild(embed);
 
-  const embed = document.createElement('iframe');
-  embed.setAttribute('src', res);
-  embed.setAttribute('width', '500');
-  embed.setAttribute('height', '500');
-  embed.setAttribute('style', 'overflow: hidden');
-  container.appendChild(embed);
+    const loader = document.getElementById('loader') as HTMLElement;
+    loader.className = 'hidden_loader';
+  }, 1000);
 });
