738d6c75cc35068f7671776ef234b4f1e4120a72
Matheus Teixeira <matheusteixeiradossantoss@gmail.com>
2021-05-19 16:10:59 -0300

    feat: add FileServices

---
+++ src/services/FilesService.ts
@@ -0,0 +1,42 @@
+import { getCustomRepository, Repository } from 'typeorm';
+import { Prescription } from '../database/models/Prescription';
+import { File } from '../database/models/File';
+import { FilesRepository } from '../repositories/FilesRepository';
+
+interface IFileCreate {
+  id?: string;
+  name: string;
+  path: string;
+  size: number;
+  type: string;
+  lastModified: number;
+  lastModifiedDate: string;
+  prescription_id?: string;
+  prescription?: Prescription;
+  created_at?: Date;
+  updated_at?: Date;
+}
+
+class FilesService {
+  private filesRepository: Repository<File>;
+
+  constructor() {
+    this.filesRepository = getCustomRepository(FilesRepository);
+  }
+
+  async create(file: IFileCreate): Promise<File> {
+    const newFile = this.filesRepository.create(file);
+    await this.filesRepository.save(newFile);
+    return newFile;
+  }
+
+  async findAllByPrescriptionId(prescription_id: string): Promise<File[]> {
+    return this.filesRepository.find({ prescription_id });
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.filesRepository.delete({ id });
+  }
+}
+
+export { FilesService, IFileCreate };
