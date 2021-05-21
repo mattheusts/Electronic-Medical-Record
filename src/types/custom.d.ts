3b5d1ad3227d55db138e0bf129f2a2bb8bdbb0ad
Matheus Teixeira <matheusteixeiradossantoss@gmail.com>
2021-05-21 18:42:11 -0300

    feat: add default global path and global mainWindow

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -1,9 +1,12 @@
+import { BrowserWindow } from 'electron';
 import Database from '../database';
 
 declare global {
   namespace NodeJS {
     interface Global {
       database: Database;
+      mainWindow: BrowserWindow;
+      DEFAULT_SAVE_PATH: string;
     }
   }
 }
