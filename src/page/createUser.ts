7ae52ee124edb3d431571ff8984b2aa7cde1a3be
Matheus Teixeira <matheusteixeiradossantoss@gmail.com>
2021-05-24 18:30:48 -0300

    refactor: syntax

--- src/page/createUser.ts
+++ src/page/createUser.ts
@@ -1,26 +1,20 @@
 import { ipcRenderer } from 'electron';
-import { User } from '../database/models/User';
+import { IUserCreate } from '../services/UsersService';
 
 window.addEventListener('load', () => {
   document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault();
     const name = document.getElementById('name') as HTMLInputElement;
-    const naturalness = document.getElementById(
-      'naturalness'
-    ) as HTMLInputElement;
+    const naturalness = document.getElementById('naturalness') as HTMLInputElement;
     const mother = document.getElementById('mother') as HTMLInputElement;
     const dad = document.getElementById('dad') as HTMLInputElement;
-    const sex = document.querySelector(
-      'input[name="sex"]:checked'
-    ) as HTMLInputElement;
+    const sex = document.querySelector('input[name="sex"]:checked') as HTMLInputElement;
     const birth = document.getElementById('birth') as HTMLInputElement;
-    const profession = document.getElementById(
-      'profession'
-    ) as HTMLInputElement;
+    const profession = document.getElementById('profession') as HTMLInputElement;
     const schooling = document.getElementById('schooling') as HTMLInputElement;
     const religion = document.getElementById('religion') as HTMLInputElement;
 
-    const req: User = {
+    const data: IUserCreate = {
       name: name.value,
       naturalness: naturalness.value,
       mother: mother.value,
@@ -32,6 +26,6 @@ window.addEventListener('load', () => {
       religion: religion.value,
     };
 
-    ipcRenderer.send('form-data', req);
+    ipcRenderer.send('form-data', data);
   });
 });
