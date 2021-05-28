a4a420a6344fcfae0096a08e7e3876b89a0bf54e
Matheus Teixeira <matheusteixeiradossantoss@gmail.com>
2021-05-28 16:53:03 -0300

    refactor: using global path as the path

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,3 @@
-import { app } from 'electron';
 import { createConnection } from 'typeorm';
 import * as path from 'path';
 import { Prescription } from './models/Prescription';
@@ -8,10 +7,7 @@ import { File } from './models/File';
 async function initDB(): Promise<void> {
   await createConnection({
     type: 'sqlite',
-    database: path.join(
-      path.join(app.getPath('userData'), 'electronic-medical-record'),
-      'Electronic-Medical-Record.sqlite'
-    ),
+    database: path.join(global.DEFAULT_SAVE_PATH, 'Electronic-Medical-Record.sqlite'),
     entities: [User, Prescription, File],
     synchronize: true,
   });
